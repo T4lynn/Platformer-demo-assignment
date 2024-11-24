5179c537187c27de65399cbd9ffdf3b6163485dd
T4lynn <148375548+T4lynn@users.noreply.github.com>
2024-11-24 18:46:27 -0500

    added good movement; started ground detection

--- Platforming movement/Assets/Scripts/PlayerController.cs
+++ Platforming movement/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public BoxCollider2D GroundedBox;
+    public float maxSpeed; 
+    public float minSpeed;
+    public float speedModifier;
+    float movementControl;
+    RaycastHit2D hit;
     public enum FacingDirection
     {
         left, right
@@ -18,33 +25,44 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //determines what the movement input is, and assigns it to movementControl. 
-        float movementControl = Input.GetAxis("Horizontal");
+        movementControl = Input.GetAxis("Horizontal");
+        Mathf.Clamp(movementControl, minSpeed, maxSpeed);
         // sets a new vector which the horizontal input float as the x value. 
-        Vector2 playerInput = new Vector2(movementControl, 0);
+        Vector2 playerInput = new Vector2(speedModifier * movementControl, 0);
         //calls movementUpdate with the previously defined vector.
         MovementUpdate(playerInput);
+        Vector2 groundedSensor = new Vector2(rb.transform.position.x, rb.transform.position.y - 0.7f);
+       hit = Physics2D.Linecast(rb.transform.position, groundedSensor);
+        //Debug.DrawLine(rb.transform.position, groundedSensor);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
         // wrote a debug.log when troubleshooting, to determine whether the
         //problem was addforce or input.getaxis. 
-        //Debug.Log("direction:" +  playerInput);
+       // Debug.Log("direction:" +  playerInput);
         //adds a force in the direction indicated by movement control, using a constant force. 
-        rb.AddForce(playerInput, ForceMode2D.Force);
+           rb.AddForce(playerInput, ForceMode2D.Force);
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (movementControl == 0)
+        {
+            return false;
+        } else { return true; }
     }
     public bool IsGrounded()
     {
-        return false;
+        if (hit) { Debug.Log("hit!"); return true; }
+        if (!hit) { return false; }
+        else { return false; }
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if (movementControl > 0) { return FacingDirection.right; }
+        if (movementControl < 0) {return FacingDirection.left; }
+        else { return FacingDirection.left;}
     }
 }
